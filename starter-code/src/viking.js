d85501f009eeb437fd176409d51cfd9b5d1591a5
Nicolas Dupouy <nicolasdupouy@gmail.com>
2018-06-17 16:03:45 +0200

    Define War

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -52,4 +52,63 @@ Saxon.prototype.receiveDamage = function(damage) {
 }
 
 // War
-function War() {}
+function War() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+}
+
+War.prototype.addViking = function(viking) {
+    this.vikingArmy.push(viking);
+}
+
+War.prototype.addSaxon = function(saxon) {
+    this.saxonArmy.push(saxon);
+}
+
+War.prototype.getRandomViking = function() {
+    return this.getRandomSoldier(this.vikingArmy);
+}
+War.prototype.getRandomSaxon = function() {
+    return this.getRandomSoldier(this.saxonArmy);
+}
+War.prototype.getRandomSoldier = function(army) {
+    let index = this.getRandomInt(army.length);
+    return army[index];
+}
+War.prototype.getRandomInt = function(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+War.prototype.vikingAttack = function() {
+    randomViking = this.getRandomViking();
+    randomSaxon = this.getRandomSaxon();
+    result = randomSaxon.receiveDamage(randomViking.attack());
+    if (result == 'A Saxon has died in combat') {
+        randomSaxonPosition = this.saxonArmy.indexOf(randomSaxon);
+        this.saxonArmy.splice(randomSaxonPosition, 1);
+    }
+    return result;
+}
+
+War.prototype.saxonAttack = function() {
+    randomViking = this.getRandomViking();
+    randomSaxon = this.getRandomSaxon();
+    result = randomViking.receiveDamage(randomSaxon.attack());
+    if (result == randomViking.name + ' has died in act of combat') {
+        randomVikingPosition = this.vikingArmy.indexOf(randomViking);
+        this.vikingArmy.splice(randomVikingPosition, 1);
+    }
+    return result;
+}
+
+War.prototype.showStatus = function() {
+    if (this.saxonArmy.length == 0) {
+        return 'Vikings have won the war of the century!';
+    }
+    else if (this.vikingArmy.length == 0) {
+        return 'Saxons have fought for their lives and survive another day...';
+    }
+    else {
+        return 'Vikings and Saxons are still in the thick of battle.';
+    }
+}
